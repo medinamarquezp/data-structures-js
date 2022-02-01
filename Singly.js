3c53a3e22f754ab9f04751be21d90a7adf1ed053
Pedro Medina Márquez <medinamarquezp@gmail.com>
2022-02-01 20:44:22 +0100

    feat: singly linked list

---
+++ Singly.js
@@ -0,0 +1,107 @@
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.next = null;
+    }
+}
+
+class Singly {
+    constructor(value = null) {
+        if (value === null)
+            throw "Singly constructor must recive a value parameter";
+        this.head = new Node(value);
+        this.tail = this.head;
+        this.size = 1;
+    }
+
+    append(value) {
+        const node = new Node(value);
+        this.tail.next = node;
+        this.tail = node;
+        this.size += 1;
+        return this;
+    }
+
+    prepend(value) {
+        const node = new Node(value);
+        node.next = this.head;
+        this.head = node;
+        this.size += 1;
+        return this;
+    }
+
+    insert(index, value) {
+        this._validateIndex(index);
+
+        // If index is on first position, set new node on head
+        if (index === 0) {
+            this.prepend(value);
+            return this;
+        }
+        // If index is on last position or greather, set new node on tail
+        if (index === this._getLastIndex() || index > this._getLastIndex()) {
+            this.append(value);
+            return this;
+        }
+
+        // Find boundary nodes
+        const { prevNode, affectedNode } = this._getBoundaries(index);
+
+        // Add new node in desired position
+        const newNode = new Node(value);
+        newNode.next = affectedNode;
+        prevNode.next = newNode;
+        this.size += 1;
+        return this;
+    }
+
+    remove(index) {
+        this._validateIndex(index);
+
+        // Find boundary nodes
+        const { prevNode, affectedNode } = this._getBoundaries(index);
+
+        // Remove affected node
+        prevNode.next = affectedNode.next;
+        this.size -= 1;
+        return this;
+    }
+
+    values() {
+        const values = [];
+        let node = this.head;
+        let iterator = 1;
+
+        while (iterator <= this.size) {
+            values.push(node.value);
+            node = node.next;
+            iterator++;
+        }
+
+        return values;
+    }
+
+    _getLastIndex() {
+        return this.size - 1;
+    }
+
+    _getBoundaries(index) {
+        const prevIndex = index > 0 ? index - 1 : index;
+        let prevNode = this.head;
+        let iterations = 0;
+
+        while (iterations !== prevIndex) {
+            prevNode = prevNode.next;
+            iterations++;
+        }
+
+        return { prevNode, affectedNode: prevNode.next };
+    }
+
+    _validateIndex(index) {
+        if (!Number.isInteger(index)) throw "index must be a valid number";
+        if (index < 0 || index > this.size) throw "index out of bounds";
+    }
+}
+
+module.exports = Singly;
---
+++ tests/Singly.test.js
@@ -0,0 +1,108 @@
+const Singly = require("../Singly");
+
+describe("Singly linked list tests", () => {
+    it("should validate constructor value", () => {
+        expect(() => new Singly()).toThrow(
+            "Singly constructor must recive a value parameter"
+        );
+    });
+
+    it("should create a Singly linked list", () => {
+        const singly = new Singly("a");
+        expect(singly.size).toBe(1);
+        expect(singly.head.value).toBe("a");
+        expect(singly.head.next).toBe(null);
+        expect(singly.tail.value).toBe("a");
+        expect(singly.tail.next).toBe(null);
+    });
+
+    it("should create a Singly linked list appending multiple nodes", () => {
+        const singly = new Singly("a");
+        singly.append("b");
+        singly.append("c");
+        expect(singly.size).toBe(3);
+        const head = singly.head;
+        expect(head.value).toBe("a");
+        const n2 = head.next;
+        expect(n2.value).toBe("b");
+        const n3 = n2.next;
+        expect(n3.value).toBe("c");
+        expect(n3.next).toBe(null);
+        expect(singly.tail.value).toBe("c");
+        expect(singly.tail.next).toBe(null);
+    });
+
+    it("should create a Singly linked list prepending one node", () => {
+        const singly = new Singly("b");
+        singly.prepend("a");
+        expect(singly.size).toBe(2);
+        expect(singly.head.value).toBe("a");
+        const n1 = singly.head.next;
+        expect(n1.value).toBe("b");
+        expect(n1.next).toBe(null);
+        expect(singly.tail.value).toBe("b");
+        expect(singly.tail.next).toBe(null);
+    });
+
+    it("should create a Singly list inserting one node on head", () => {
+        const singly = new Singly("b");
+        singly.insert(0, "a");
+        expect(singly.size).toBe(2);
+        expect(singly.head.value).toBe("a");
+        const n1 = singly.head.next;
+        expect(n1.value).toBe("b");
+        expect(n1.next).toBe(null);
+        expect(singly.tail.value).toBe("b");
+        expect(singly.tail.next).toBe(null);
+    });
+
+    it("should create a Singly list inserting one node on tail", () => {
+        const singly = new Singly("a");
+        singly.insert(1, "b");
+        expect(singly.size).toBe(2);
+        expect(singly.head.value).toBe("a");
+        const n1 = singly.head.next;
+        expect(n1.value).toBe("b");
+        expect(n1.next).toBe(null);
+        expect(singly.tail.value).toBe("b");
+        expect(singly.tail.next).toBe(null);
+    });
+
+    it("should create a Singly list inserting one node on custom place", () => {
+        const singly = new Singly("a");
+        singly.append("c");
+        singly.append("d");
+        expect(singly.size).toBe(3);
+        singly.insert(1, "b");
+        expect(singly.size).toBe(4);
+        expect(singly.head.value).toBe("a");
+        const n1 = singly.head.next;
+        expect(n1.value).toBe("b");
+        const n2 = n1.next;
+        expect(n2.value).toBe("c");
+        const n3 = n2.next;
+        expect(n3.value).toBe("d");
+        expect(n3.next).toBe(null);
+    });
+
+    it("should create a Singly list and remove a node", () => {
+        const singly = new Singly("a");
+        singly.append("b");
+        singly.append("d");
+        expect(singly.size).toBe(3);
+        singly.remove(2);
+        expect(singly.size).toBe(2);
+        expect(singly.head.value).toBe("a");
+        const n1 = singly.head.next;
+        expect(n1.value).toBe("b");
+        expect(n1.next).toBe(null);
+    });
+
+    it("should create a Singly list and return all node values", () => {
+        const singly = new Singly("a");
+        singly.append("b");
+        singly.append("c");
+        expect(singly.size).toBe(3);
+        expect(singly.values()).toEqual(["a", "b", "c"]);
+    });
+});
